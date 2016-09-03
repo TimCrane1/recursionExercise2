8bd93df874111e70591bff439422542932e714fb
Tim Crane <mickeymouth82@gmail.com>
2016-09-03 14:21:24 -0400

    code clean-up

--- recursionExercise2.js
+++ recursionExercise2.js
@@ -1,11 +1,11 @@
 var tools = ["computer", "books", "text editor"];
 
-function listArray(arr, count) {
+function listArray (arr, count) {
     if (count === undefined) {
         count = 0;
     }
     console.log(arr[count]);
-    if (count ===arr.length - 1) {
+    if (count === arr.length - 1) {
         return "done";
     }
     else {
